3e575ef0b1f3d41328827474ff1cd1b9a910b5e9
maurexfontys <m.waelen@student.fontys.nl>
2022-10-18 19:58:02 +0200

    added assets

--- Portal/src/app/@core/core.module.ts
+++ Portal/src/app/@core/core.module.ts
@@ -9,11 +9,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {NgbCollapseModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { FilterPanelComponent } from './components/filter-panel/filter-panel.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent, FilterPanelComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, FilterPanelComponent, ModalComponent, BreadcrumbsComponent],
   imports: [CommonModule, RouterModule, NgbCollapseModule, NgbModule]
+
 })
 export class coreModule {
   constructor(@Optional() @SkipSelf() parentModule: coreModule) {
