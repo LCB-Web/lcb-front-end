f2f2cb2e745d61c620ac4dffd130b86e03fa871b
Eva Abbenhuis <e.abbenhuis@student.fontys.nl>
2022-11-01 18:30:32 +0100

    Event element, Maurice kijkt nog naar tekst en schaalbaarheid

--- Portal/src/app/@core/core.module.ts
+++ Portal/src/app/@core/core.module.ts
@@ -10,10 +10,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import {NgbCollapseModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { EventsComponent } from './components/events/events.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { FilterPanelComponent } from './components/filter-panel/filter-panel.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent, EventsComponent, BlogComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, EventsComponent, BlogComponent, FilterPanelComponent, ModalComponent, BreadcrumbsComponent],
   imports: [CommonModule, RouterModule, NgbCollapseModule, NgbModule]
 
 })
