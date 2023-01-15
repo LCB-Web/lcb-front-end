534b4d7843991864e59edad7485cad2a5fe58784
Marina Veldhuizen <marina.veldhuizen@student.fontys.nl>
2023-01-15 23:04:58 +0100

    final

--- Portal/src/app/@shared/components/new-reads/new-reads.component.ts
+++ Portal/src/app/@shared/components/new-reads/new-reads.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 // import Swiper core and required modules
 import SwiperCore, { SwiperOptions, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { DataService } from 'src/app/data.service';
 
 // install Swiper modules
 SwiperCore.use([Navigation, Scrollbar, A11y]);
@@ -12,11 +13,22 @@ SwiperCore.use([Navigation, Scrollbar, A11y]);
   encapsulation: ViewEncapsulation.None,
 })
 export class NewReadsComponent implements OnInit {
+  title = 'blog';
+  data: any;
+
+
+
+ 
+
   config: SwiperOptions = {
     slidesPerView: 2.5,
+    effect: 'fade',
     spaceBetween: 30,
     navigation: false,
-    pagination: { clickable: true },
+    pagination: { 
+      type: 'bullets',
+      clickable: true 
+    },
     scrollbar: { draggable: true },
   };
   onSwiper(e:any) {
@@ -25,9 +37,24 @@ export class NewReadsComponent implements OnInit {
   onSlideChange() {
     console.log('slide change');
   }
-  constructor() { }
+  constructor(private user:DataService) {
 
+    data!: Data[]
+    dataSelected: string  
+
+    this.user.getData().subscribe(data=> {
+      console.warn(data)
+      this.data = data
+    })
+   }
+  
   ngOnInit(): void {
+    this.fetchData();
+  }
+
+  private fetchData () {
+this.Datasevice.getData().subscribe(data =>)
+this.data = data
   }
 
 }
